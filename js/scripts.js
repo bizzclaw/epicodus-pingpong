afdb683d43659fa358e24f004bd7df9c7b998afa
bizzclaw <bizzclaw1@gmail.com>
2017-09-22 13:43:18 -0700

    Add error messages for values < 1 and > 100

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-// *********BACKEND********* //
+// *********  BACKEND  ********* //
 var pings = [15,5,3]
 var pongs = ["ping-pong", "pong", "ping"]
 
@@ -10,9 +10,29 @@ var getPong = function(number) {
 	}
 	return number
 }
-// *********ANIME********* //
+// *********  ANIME  ********* //
 
-function flyNumbers(amt) {
+var flyOutput = function(output, i) {
+	var id = "number-"+i
+	$("#output").append(`<div class="flynumber" id="`+ id +`">`+ output +`</div>`);
+
+	var animedata = {
+		targets: "#"+id,
+		translateX: "50%",
+		translateY: 10,
+		opacity: 1,
+		delay: function() {
+			return i * 100;
+		},
+	}
+	if (typeof output === "string") {
+		animedata.rotate = "2turn"
+		animedata.color = "#FF8D33"
+	}
+	anime(animedata);
+}
+
+var flyNumbers = function(amt, error) {
 	var fadetime = 300;
 	$(".flynumber").fadeOut(fadetime)
 	setTimeout(function(){
@@ -22,37 +42,21 @@ function flyNumbers(amt) {
 			opacity: 1,
 			translateY: 50
 		})
-		for (i = 1; i <= amt; i++) {
-			var id = "number-"+i
-			var ponged = getPong(i)
-			$("#output").append(`<div class="flynumber" id="`+ id +`">`+ ponged +`</div>`);
-
-			var animedata = {
-				targets: "#"+id,
-				translateX: "50%",
-				translateY: 10,
-				opacity: 1,
-				delay: function() {
-					return i * 100;
-				},
-			}
-			if (typeof ponged === "string") {
-				animedata.rotate = "4turn"
-				animedata.color = "#FF8D33"
-			}
-			anime(animedata);
+		if (error) {
+			flyOutput(error,0)
 		}
-	}, fadetime)
+		for (i = 1; i <= amt; i++) {
+			flyOutput(getPong(i), i);
+		}
+	}, fadetime);
 }
 
-
-// *********JQUERY********* //
+// *********  JQUERY  ********* //
 $(document).ready(function() {
 	anime({
 		targets: "#main-header",
 		opacity: 1,
 		scale: 1.3,
-		// rotate: "2turn"
 	});
 
 	anime({
@@ -63,6 +67,14 @@ $(document).ready(function() {
 	$("form").submit(function(event) {
 		event.preventDefault();
 		var input = parseInt($("input").val());
-		flyNumbers(input)
+		var error;
+		if (input > 100) {
+			error = "Ammount too high, clamped to 100.";
+			input = 100;
+		} else if (input < 1) {
+			input = 1;
+			error = "Ammount to low, forced to 1."
+		}
+		flyNumbers(input, error)
 	});
 });
